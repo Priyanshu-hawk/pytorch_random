1111f5b107066f90f70c9d4c5d168623eb41bff6
Priyanshu-hawk <priyanshupatel.hawk@gmail.com>
2024-12-13 00:24:10 +0530

    code update

--- Transformer_Scratch/model.py
+++ Transformer_Scratch/model.py
@@ -118,4 +118,27 @@ class ResidualConnection(nn.Module):
     def forward(self, x, sublayer):
         return x + self.dropout(sublayer(self.norm(x)))
     
+class EncoderBloack(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttention, feed_forward: FeedForwardBloack, dropout: float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.fead_forward = feed_forward
+        self.residual_connection = nn.ModuleDict([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connection[0][x, lambda x: self.self_attention_block(x,x,x, src_mask)]
+        x = self.residual_connection[1](x, self.fead_forward)
+        return x
     
+class EncoderBlock(nn.Module):
+
+    def __init__(self, layer: nn.ModuleList):
+        super().__init__()
+        self.layers = layer
+        self.layer_norm = LayerNormalization()
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+
+        return self.layer_norm(x)
